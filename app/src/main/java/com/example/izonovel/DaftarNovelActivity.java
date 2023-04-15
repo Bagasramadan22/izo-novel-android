f0c35c3e3d1440eb492881e7ea89abdcf970bf42
Bagasramadan22 <ramadanbagas6@gmail.com>
2023-04-15 17:10:23 +0700

    daftar novel

---
+++ app/src/main/java/com/example/izonovel/DaftarNovelActivity.java
@@ -0,0 +1,14 @@
+package com.example.izonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaftarNovelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_novel);7
+    }
+}
